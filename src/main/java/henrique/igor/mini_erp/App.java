ebe26db914f97a48d5867747cb098230f7056ed9
igorhghd <igorhenrique.gomes@hotmail.com>
2026-02-27 18:32:04 -0300

    initial config and created packages

---
+++ src/main/java/henrique/igor/mini_erp/App.java
@@ -0,0 +1,13 @@
+package henrique.igor.mini_erp;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
